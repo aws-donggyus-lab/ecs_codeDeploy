29ebd6074e1e5674c3a6852dca6ec2bc6297971a
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-08-20 23:46:39 +0900

    wip

--- apiserver/src/healthCheck/index.controller.ts
+++ apiserver/src/healthCheck/index.controller.ts
@@ -7,24 +7,11 @@ class HealthCheckController {
   constructor() {
     this.router = Router()
     this.router.get('/health', reqMiddleware, this.healthCheck)
-    this.router.get('/environment', reqMiddleware, this.outputEnv)
   }
 
   async healthCheck(req: Request, res: Response) {
-    console.log('success ===> new version 1.0')
-    return res.status(200).json('success')
-  }
-
-  async outputEnv(req: Request, res: Response) {
-    console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'))
-    console.log({
-      port: process.env.PORT,
-      name: process.env.NAME,
-      age: process.env.AGE,
-      per: process.env.PER,
-    })
-
     return res.status(200).json({
+      version: process.env.VERSION,
       date: dayjs().format('YYYY-MM-DD HH:mm:ss'),
       port: process.env.PORT,
       name: process.env.NAME,
